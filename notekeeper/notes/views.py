93aa60be6e8c218504ce4254979cf3c7bd6a895e
Omkar Pathak <omkarpathak27@gmail.com>
2019-11-18 09:02:50 +0530

    Bug fix for photos not included while note donwload

--- notekeeper/notes/views.py
+++ notekeeper/notes/views.py
@@ -2,13 +2,41 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Note, AddNoteForm
 from django.contrib import messages
-import json
+import json, os
 from datetime import datetime, timedelta 
 from django.core.paginator import Paginator
 from django.http import JsonResponse, HttpResponse
 from io import BytesIO
+from django.conf import settings
 from django.template.loader import get_template
 from xhtml2pdf import pisa
+
+
+def link_callback(uri, rel):
+    """
+    Convert HTML URIs to absolute system paths so xhtml2pdf can access those
+    resources
+    """
+    # use short variable names
+    sUrl = settings.STATIC_URL      # Typically /static/
+    sRoot = settings.STATIC_ROOT    # Typically /home/userX/project_static/
+    mUrl = settings.MEDIA_URL       # Typically /static/media/
+    mRoot = settings.MEDIA_ROOT     # Typically /home/userX/project_static/media/
+
+    # convert URIs to absolute system paths
+    if uri.startswith(mUrl):
+        path = os.path.join(mRoot, uri.replace(mUrl, ""))
+    elif uri.startswith(sUrl):
+        path = os.path.join(sRoot, uri.replace(sUrl, ""))
+    else:
+        return uri  # handle absolute uri (ie: http://some.tld/foo.png)
+
+    # make sure that file exists
+    if not os.path.isfile(path):
+            raise Exception(
+                'media URI must start with %s or %s' % (sUrl, mUrl)
+            )
+    return path
 
 
 def render_to_pdf(template_src, context_dict={}):
@@ -18,7 +46,7 @@ def render_to_pdf(template_src, context_dict={}):
     template = get_template(template_src)
     html  = template.render(context_dict)
     result = BytesIO()
-    pdf = pisa.pisaDocument(BytesIO(html.encode("UTF-8")), result)
+    pdf = pisa.pisaDocument(BytesIO(html.encode("UTF-8")), result, link_callback=link_callback)
     if not pdf.err:
         return HttpResponse(result.getvalue(), content_type='application/pdf')
     return HttpResponse("Error Rendering PDF", status=400)
